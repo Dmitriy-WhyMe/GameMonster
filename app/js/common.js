f2b2c3d05b8cf5f2ed196cde9c110fb2a71f203c
Dmitriy <dima.ivahnoff@mail.ru>
2022-04-15 00:24:43 +0300

    slider-top finish

--- app/js/common.js
+++ app/js/common.js
@@ -10,5 +10,66 @@
 			}
 		})
 
+		var swiper = new Swiper(".mySwiper", {
+			slidesPerView: 1,
+			spaceBetween: 500,
+			loop: true,
+			freeMode: true,
+			watchSlidesProgress: true,
+			pagination: {
+				el: ".swiper-pagination",
+			},
+			breakpoints: {
+				310: {
+					slidesPerView: 1,
+					allowTouchMove: true,
+					spaceBetween: 50,
+				},
+				1025: {
+					speed: 1000,
+					spaceBetween: 500,
+					slidesPerView: 1,
+					allowTouchMove: false,
+				},
+			}
+		});
+		var swiper2 = new Swiper(".mySwiper-1", {
+			slidesPerView: 5,
+			spaceBetween: 30,
+			grabCursor: true,
+			loop: true,
+			speed: 1000,
+			autoplay: {
+				delay: 5000,
+			},
+			thumbs: {
+				swiper: swiper,
+			},
+			breakpoints: {
+				310: {
+					slidesPerView: 1,
+					autoplay: false,
+				},
+				768: {
+					slidesPerView: 3,
+				},
+				1024: {
+					slidesPerView: 3,
+				},
+				1280: {
+					slidesPerView: 4,
+					spaceBetween: 38,
+				},
+				1366: {
+					spaceBetween: 15,
+					slidesPerView: 5,
+				},
+				1441: {
+					spaceBetween: 30,
+					slidesPerView: 5,
+				},
+			}
+		});
+
 	});
 })(jQuery);
